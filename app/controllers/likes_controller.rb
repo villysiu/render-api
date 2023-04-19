722210f5aabbeb23528991c816d9241b3d5e4b72
Villy Siu <villysiu@gmail.com>
2023-04-19 10:54:06 -0700

    add jbilder gem

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,27 @@
 class LikesController < ApplicationController
+    # before_action :authenticate_user!, :except => [:index]
+    before_action :find_photo
+    # before_action :find_like, :only => [:destroy]
+    def index
+        @likes=@photo.likes
+    end
+    def create
+        @like= @photo.likes.create!(user_id: current_user.id)
+        render json: {id: @like.id, user_id: @like.user_id}, status: :ok
+    end
+
+    def destroy
+        @like=@photo.likes.find_by(user_id: current_user.id)
+        @like.destroy
+        render json: @like.id, status: :ok
+    end
+
+    private
+
+    def find_photo
+        @photo=Photo.find(params[:photo_id])
+    end
+
+    
+
 end

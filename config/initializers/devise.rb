1c99b9659cc7b59133d0d85c795c6afd31b85324
Villy Siu <villysiu@gmail.com>
2023-04-26 10:05:58 -0700

    correct devise jwt secret key format

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -315,7 +315,8 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     # jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
-    jwt.secret =  Rails.application.credentials.dig(:DEVISE_JWT_SECRET_KEY)
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    # jwt.secret =  Rails.application.credentials.dig(:DEVISE_JWT_SECRET_KEY)
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}],
     ]
